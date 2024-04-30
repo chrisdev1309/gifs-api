d3751f30073baa7224bf6030321bcbb6e22f047f
Christiam Mori <christiam1309@gmail.com>
2024-04-30 18:20:20 -0500

    capitulo - Leer del localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,11 @@ export class GifsService {
     private apiKey:         string   = 'eEcbH0Aa1UeAZPNpOhbiNjOM7xSDptlN';
     private serviceUrl:     string   = 'https://api.giphy.com/v1/gifs';
 
-    constructor(private hhtp: HttpClient) { }
+    constructor(private hhtp: HttpClient) {
+        this.loadLocalStorage();
+        console.log('Gifs Service Ready');
+     }
+
 
     get tagsHistory() {
         return [...this._tagsHistory];
@@ -27,10 +31,26 @@ export class GifsService {
 
         this._tagsHistory.unshift( tag );
         this._tagsHistory = this.tagsHistory.splice(0,10);
-
+        this.saveLocalStorage();
     }
 
 
+    private saveLocalStorage(): void {
+        localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+    }
+
+
+    private loadLocalStorage():void {
+        if ( ! localStorage.getItem('history')) return;
+
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+        if ( this._tagsHistory.length === 0 ) return;
+
+        this.searchTag( this._tagsHistory[0] );
+    }
+
+    
     searchTag( tag: string):void {
         if ( tag.length === 0) {
             return
@@ -49,7 +69,6 @@ export class GifsService {
                 this.gifList = resp.data;
 
             });
-
     }
     
 }
